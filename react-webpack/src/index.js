f4dc272a25ee434aaf1afe0f4df80e5cebe7f4d0
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-06-21 12:02:49 -0300

    iniciado react de forma rudimentar

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,1 +1,10 @@
-console.log('webpack esta funcionando!!');
+'use strict';
+
+var Title = require('./app');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
